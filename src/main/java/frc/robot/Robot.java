3d7f28a6ff31f83a78cf81e65a5451ec6f4e48bc
Steven Bowman <prongs95@gmail.com>
2019-01-04 09:28:58 -0600

    Added a comment
    The comment is on line 8 and references the type of file

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,8 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+// This is Robot.java
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
